53ff65025b0cf5c5402c2ab289bfbf4deff2220b
Atharva Agrawal <137732024+23xAthz@users.noreply.github.com>
2026-02-01 23:38:34 +0530

    Create solution.c

---
+++ problems/3010. Divide an Array Into Subarrays With Minimum Cost I/solution.c
@@ -0,0 +1,18 @@
+#include<stdio.h
+#include <limits.h>
+
+int minimumCost(int* nums, int numsSize) {
+    int min1 = 1000;
+    int min2 = 1000;
+    
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] < min1) {
+            min2 = min1;
+            min1 = nums[i];
+        } else if (nums[i] < min2) {
+            min2 = nums[i];
+        }
+    }
+    
+    return nums[0] + min1 + min2;
+}
